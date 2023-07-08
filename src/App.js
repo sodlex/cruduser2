55768894673d47a0510fd212d8ab5659b7521dce
sodlex <51163558+sodlex@users.noreply.github.com>
2023-07-08 18:24:22 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CrudUser from "./components/CrudUser";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CrudUser />
+    </div>
+  );
+}
+
+export default App;
